c0aec4d469ade81adf1d18efbc5622f6947cdc41
Bushra Abad <bushra.abad@example.com>
2026-05-13 21:21:28 +0500

    Configure backend to listen in production for deployment

--- ats-final/backend/server.js
+++ ats-final/backend/server.js
@@ -113,10 +113,8 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== 'production') {
-  server.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
-  });
-}
+server.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
+});
 
 module.exports = app;
